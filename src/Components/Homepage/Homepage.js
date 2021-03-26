9c89c73746284c7dbe0063f1b599e3c467bdf5c8
SHresTho12 <talimul12bari@gmail.com>
2021-03-26 20:44:06 +0600

    Adding basic navbar for homepag using react-bootstrap

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../NavBar/NavBar';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Homepage;
